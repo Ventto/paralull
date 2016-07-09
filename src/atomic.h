43491a110dcbf15926f521291c2bacedfd890041
Snaipe <franklinmathieu@gmail.com>
2016-07-09 19:01:56 +0200

    pll: Added atomics

---
+++ src/atomic.h
@@ -0,0 +1,8 @@
+#ifndef ATOMIC_H_
+# define ATOMIC_H_
+
+# define pll_cas(Ptr, Val, Newval) (__sync_bool_compare_and_swap((Ptr), (Val), (Newval)))
+# define pll_faa(Ptr, Val) (__sync_fetch_and_add((Ptr), (Val)))
+# define pll_fas(Ptr, Val) (__sync_fetch_and_sub((Ptr), (Val)))
+
+#endif /* !ATOMIC_H_ */
