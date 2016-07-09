c3a94facdf5aaae3c517070955568d45b6a6f010
Thomas Venries <thomas.venries@gmail.com>
2016-07-09 23:02:39 +0200

    pll: Back to the first prototype

--- include/paralull.h
+++ include/paralull.h
@@ -8,7 +8,7 @@ typedef struct pll_queue *pll_queue;
 
 pll_queue pll_queue_init(void);
 void pll_queue_term(pll_queue q);
-void pll_enqueue(pll_queue q, void *val, uint64_t cell_id);
+void pll_enqueue(pll_queue q, void *val);
 void *pll_dequeue(pll_queue q);
 bool pll_queue_empty(pll_queue q);
 
